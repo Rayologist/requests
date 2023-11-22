4d1f8e43992f547ceae0c9231bb7688a5d397d3f
Bowei Chen <bwchen.dev@gmail.com>
2023-11-23 01:30:32 +0800

    Add test for response handlers

--- tests/request.test.ts
+++ tests/request.test.ts
@@ -4,9 +4,21 @@ import { request } from '../src';
 jest.mock('node-fetch');
 
 describe(__filename, () => {
+  const raw = (): Record<string, string[]> => ({
+    'content-type': ['application/json'],
+  });
+
+  const headers = {
+    raw,
+    get: (name: string) => {
+      return raw()[name.toLowerCase()];
+    },
+  };
+
   it('should launch get request', async () => {
     (fetch as any).mockResolvedValue({
       json: () => Promise.resolve({ userId: 1 }),
+      headers,
       ok: true,
       status: 200,
     });
@@ -20,21 +32,12 @@ describe(__filename, () => {
   });
 
   it('should throw error', async () => {
-    const raw = (): Record<string, string[]> => ({
-      'content-type': ['application/json'],
-    });
-
     (fetch as any).mockResolvedValue({
       json: () => Promise.resolve({ error: 'not found' }),
       text: () => Promise.resolve(''),
       ok: false,
       status: 404,
-      headers: {
-        raw,
-        get: (name: string) => {
-          return raw()[name.toLowerCase()];
-        },
-      },
+      headers,
     });
 
     const [data, err] = await request({
@@ -61,6 +64,74 @@ describe(__filename, () => {
     };
 
     expect(data).toBeNull();
-    expect(error).toStrictEqual(thrown);
+    expect(thrown).toStrictEqual(error);
+  });
+
+  it('should correctly handle successful responses with a custom success handler', async () => {
+    (fetch as any).mockResolvedValue({
+      json: () => Promise.resolve({ userId: 1 }),
+      headers,
+      ok: true,
+      status: 200,
+    });
+
+    const [data, err] = await request(
+      {
+        url: 'https://example.com',
+        method: 'GET',
+      },
+      {
+        handleSuccessResponse: async (response) => {
+          return 123;
+        },
+      },
+    );
+
+    expect(data).toEqual(123);
+    expect(err).toBeNull();
+  });
+
+  it('should correctly handle failed responses with a custom error handler', async () => {
+    const buffer = Buffer.from('some error message');
+    (fetch as any).mockResolvedValue({
+      json: () => Promise.resolve({ error: 'not found' }),
+      text: () => Promise.resolve(''),
+      ok: false,
+      status: 404,
+      headers,
+    });
+
+    const [data, err] = await request(
+      {
+        url: 'https://example.com',
+        method: 'POST',
+      },
+      {
+        handleErrorResponse: async (response) => {
+          return buffer;
+        },
+      },
+    );
+
+    const error = {
+      data: buffer,
+      headers: { 'content-type': ['application/json'] },
+      method: 'POST',
+      statusCode: 404,
+      timestamp: expect.any(Number),
+      url: 'https://example.com',
+    };
+
+    const thrown = {
+      data: err?.data,
+      headers: err?.headers,
+      method: err?.method,
+      statusCode: err?.statusCode,
+      timestamp: err?.timestamp,
+      url: err?.url,
+    };
+
+    expect(data).toBeNull();
+    expect(thrown).toStrictEqual(error);
   });
 });
